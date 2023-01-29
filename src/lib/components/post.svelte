286660861a2f2eb94c82ef64c61e3f5e0af854ce
sabrina <sabrinaabrodi20@gmail.com>
2023-01-29 18:58:09 +0100

    styling post

--- src/lib/components/post.svelte
+++ src/lib/components/post.svelte
@@ -10,14 +10,13 @@
             };
 }
 </script>
-
-<div class="app">
+<div class="flex place-items-center justify-center flex-col">
 	<h1>Upload Image</h1>
   
         {#if avatar}
-        <img class="avatar" src="{avatar}" alt="d" />
+        <img class="flex w-[50px] h-[50px]" src="{avatar}" alt="d" />
         {:else}
-        <div class="bg-gray-100 w-96"> <img class="avatar upload" src="https://static.thenounproject.com/png/625182-200.png" alt="" on:click={()=>{fileinput.click();}} />
+        <div class="bg-gray-100 w-96"> <img class="flex w-[50px] h-[50px] cursor-pointer" src="https://static.thenounproject.com/png/625182-200.png" alt="" on:click={()=>{fileinput.click();}} />
  </div>
        {/if}
 				
